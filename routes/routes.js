61679dca0ea9e32887d82c70ea35e2208c0c9756
Carlos de Castro <ccastro@ipca.pt>
2015-03-18 23:42:44 +0000

    Fifth step Session Management

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,15 @@ module.exports = function(express, app){
     router.get('/chatrooms', function(req, res, next){
         res.render('chatrooms', {title: 'Chatrooms'});
     })
+    
+    router.get('/setcolor', function(req, res, next){
+        req.session.favColor = "Red";
+        req.send('Setting favourite color');
+    })
+    
+    router.get('/getcolor', function(req, res, next){
+        res.send('Favourite Color: ' + (req.session.favColor===undefined?"Not Found":req.session.favColor));
+    })
 
     app.use('/', router);
     
