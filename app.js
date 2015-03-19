7e1996db289e079d1622365405aa20a6e0ab9612
Carlos de Castro <ccastro@ipca.pt>
2015-03-19 00:21:54 +0000

    Sixth step

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ var express = require('express'),
     app = express(),
     path = require('path'),
     cookieParser = require('cookie-parser'),
-    session = require('express-session')
+    session = require('express-session'),
+    config = require('./config/config.js'),
+    ConnectMongo = require('connect-mongo')(session)
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(session({secret:'lesirocks', saveUninitialized:true, resave:true}));
+var env = process.env.NODE_ENV || 'development';
+
+if(env === 'development'){
+    //dev specific settings
+    app.use(session({secret:config.sessionSecret}));
+}else{
+    //prodution specific settings
+    app.use(session({
+        secret:config.sessionSecret, 
+        store: new ConnectMongo({
+            url:config.dbURL,
+            stringify: true
+        })
+    }));
+}
 
 require('./routes/routes.js')(express, app);
 
 app.listen(3000, function(){
     console.log('LESIchat Working on Port 3000');
+    console.log('Mode: ' + env);
 })
